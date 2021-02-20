bc2b8ff36b0e5b139913b5ba09e03151cae2f2dc
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-02-20 15:39:14 +0100

    LifeTheUniverseAndEverything

---
+++ LifeTheUniverseAndEverything/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//using Helpers;
+
+namespace LifeTheUniverseAndEverything {
+    public class Program {
+        static void Main(string[] args) {
+            //ConsoleHelper.RedirectInputToFile();
+            while (true) {
+                var number = Convert.ToInt32(Console.ReadLine());
+                if (number == 42) {
+                    break;
+                }
+
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
