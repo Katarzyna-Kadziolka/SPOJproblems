ad1e0a990ffffc4c1de9b595252d0a4cf7637c79
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-02-26 15:02:44 +0100

    SortingBankAccounts

--- SortingBankAccounts/SortingBankAccounts.cs
+++ SortingBankAccounts/SortingBankAccounts.cs
@@ -1,56 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 
 namespace SortingBankAccounts {
     public class SortingBankAccounts {
         static void Main(string[] args) {
             Helpers.ConsoleHelper.RedirectInputToFile();
-            List<int> accounts = new List<int>();
-            string line;
-            do {
-                line = Console.ReadLine();
-                if (string.IsNullOrEmpty(line)) {
-                    if (line == string.Empty) {
-                        Console.WriteLine("");
-                    }
-                }
-                else {
-                    if (line.Length != 26) {
-                        continue;
-                    }
-
-                    if (accounts.Count == 0) {
-                        accounts.Add(Convert.ToInt32(line));
-                    }
-                    else {
-                        for (var index = 0; index < accounts.Count; index++) {
-                            var accountNumber = accounts[index];
-                            var numberArray = accountNumber.ToString().ToCharArray();
-                            for (var i = 0; i < line.Length; i++) {
-                                if (line[i] == numberArray[i]) {
-                                    
-                                    continue;
-                                }
-
-                                if (line[i] > numberArray[i]) {
-                                    accounts.Insert(index+1, Convert.ToInt32(line));
-                                    break;
-                                }
-
-                                else {
-                                    accounts.Insert(index-1, Convert.ToInt32(line));
-                                    break;
-                                }
-                            }
-                        }
-                    }
+            int numberOfTests = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < numberOfTests; i++) {
+                int numberOfAccountsInTest = Convert.ToInt32(Console.ReadLine());
+                List<string> accounts = new List<string>();
+                for (int j = 0; j < numberOfAccountsInTest; j++) {
+                    accounts.Add(Console.ReadLine());
                 }
 
-            } while (line != null);
+                var sortedAccount = accounts.GroupBy(a => a)
+                    .Select(a => $"{a.Key} {a.Count()}")
+                    .OrderBy(a => a);
+                foreach (var account in sortedAccount) {
+                    Console.WriteLine(account);
+                }
+
+                Console.WriteLine();
+                Console.ReadLine();
+            }
         }
     }
 }
-// nie uwzględniłam przerw w liczbach .. chyba, czy spacja ma swój symbol char?
-// trzeba dodać spację i cyfrę liczącą powtórzenia
