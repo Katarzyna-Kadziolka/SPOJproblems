0562b261b9e09529c194ac433388439e07d96a74
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-03-18 14:30:37 +0100

    Sum

---
+++ Sum/Sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sum {
+    public class Sum {
+        static void Main(string[] args) {
+            Helpers.ConsoleHelper.RedirectInputToFile();
+            var newNum = Console.ReadLine();
+            var sum = 0;
+            while (!string.IsNullOrEmpty(newNum)) {
+                sum = sum + Convert.ToInt32(newNum);
+                newNum = Console.ReadLine();
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
