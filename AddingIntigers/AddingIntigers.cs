0c36f5db8580b0684ccd609dc858cd2cda73a7d8
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-03-18 14:16:10 +0100

    AddingIntigers

---
+++ AddingIntigers/AddingIntigers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddingIntigers {
+    public class AddingIntigers {
+        static void Main(string[] args) {
+            Helpers.ConsoleHelper.RedirectInputToFile();
+            var sum = 0;
+            for (int i = 0; i < 3; i++) {
+                sum = sum + Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
