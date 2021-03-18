666e2fb0407c9af61a390464066c321f428487ba
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-03-18 14:45:29 +0100

    MultipleSum

---
+++ MultipleSum/MultipleSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace MultipleSum {
+    public class MultipleSum {
+        static void Main(string[] args) {
+            Helpers.ConsoleHelper.RedirectInputToFile();
+            var line = Console.ReadLine();
+            long sum = 0;
+            while (!string.IsNullOrEmpty(line)) {
+                var numbers = line.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(a => Int64.Parse(a.Trim())).ToList();
+                ;
+                Console.WriteLine(numbers.Sum());
+                sum = sum + numbers.Sum();
+                line = Console.ReadLine();
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
