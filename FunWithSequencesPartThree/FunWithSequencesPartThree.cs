23663e3ce9947a7e01586941995696b8779f2f9b
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-04-01 15:13:49 +0200

    Fun with sequences part two and start part three

---
+++ FunWithSequencesPartThree/FunWithSequencesPartThree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace FunWithSequencesPartThree {
+    class FunWithSequencesPartThree {
+        static void Main(string[] args) {
+            Helpers.ConsoleHelper.RedirectInputToFile();
+            var line = Console.ReadLine();
+            while (!string.IsNullOrEmpty(line)) {
+                var firstArray = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries),
+                    int.Parse);
+                Console.ReadLine();
+                var secondArray = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries),
+                    int.Parse);
+                var filteredNumbers = firstArray.Where(a => secondArray.Any(b => b == a))
+                    .OrderBy(a => a)
+                    .ToList();
+                var indexes = filteredNumbers.Select(a => firstArray.ToList().IndexOf(a) + 1).ToArray();
+                var answerLine = string.Join(" ", indexes.Select(a => a.ToString()).ToArray());
+
+                Console.WriteLine(answerLine);
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ FunWithSequencesPartTwo/FunWithSequencesPartTwo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace FunWithSequencesPartTwo {
+    class FunWithSequencesPartTwo {
+        static void Main() {
+            Helpers.ConsoleHelper.RedirectInputToFile();
+            var line = Console.ReadLine();
+            while (!string.IsNullOrEmpty(line)) {
+                var firstArray = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries),
+                    int.Parse);
+                Console.ReadLine();
+                var secondArray = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries),
+                    int.Parse);
+                var filteredNumbers = firstArray.Where(a => secondArray.Any(b => b == a))
+                    .OrderBy(a => a)
+                    .ToArray();
+                var answerLine = string.Join(" ", filteredNumbers.Select(a => a.ToString()).ToArray());
+
+                Console.WriteLine(answerLine);
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
