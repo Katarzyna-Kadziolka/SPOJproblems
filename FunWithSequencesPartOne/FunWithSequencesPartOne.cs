1175784a28f3c69092e07d86f5b666fa03e6da23
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-04-01 14:43:08 +0200

    FunWithSequencesPartOne

---
+++ FunWithSequencesPartOne/FunWithSequencesPartOne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace FunWithSequencesPartOne {
+    class FunWithSequencesPartOne {
+        static void Main() {
+            Helpers.ConsoleHelper.RedirectInputToFile();
+            var line = Console.ReadLine();
+            while (!string.IsNullOrEmpty(line)) {
+                var firstArray = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+                Console.ReadLine();
+                var secondArray = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+                var filteredNumbers = firstArray.Where(a => !secondArray.Any(b => b == a))
+                    .OrderBy(a => a)
+                    .ToArray();
+                var answerLine = string.Join(" ", filteredNumbers.Select(a => a.ToString()).ToArray());
+
+                Console.WriteLine(answerLine);
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
