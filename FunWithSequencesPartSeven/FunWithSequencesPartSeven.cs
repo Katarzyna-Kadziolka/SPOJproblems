4ddf3fdff90690aee25e80b31e9f47ed349e8f34
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-04-07 16:01:33 +0200

    Six and seven

---
+++ FunWithSequencesPartSeven/FunWithSequencesPartSeven.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace FunWithSequencesPartSeven
+{
+    class FunWithSequencesPartSeven
+    {
+        static void Main()
+        {
+            Helpers.ConsoleHelper.RedirectInputToFile();
+            var line = Console.ReadLine();
+            while (!string.IsNullOrEmpty(line)) {
+                var firstArray = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries),
+                    double.Parse);
+                Console.ReadLine();
+                var secondArray = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries),
+                    double.Parse);
+                string answer;
+                if (firstArray.Sum()/firstArray.Length > secondArray.Sum()/secondArray.Length) {
+                    answer = string.Join(" ", firstArray);
+                }
+                else {
+                    answer = string.Join(" ", secondArray);
+                }
+
+                Console.WriteLine(answer);
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ FunWithSequencesPartSix/FunWithSequencesPartSix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace FunWithSequencesPartSix {
+    class FunWithSequencesPartSix {
+        static void Main() {
+            Helpers.ConsoleHelper.RedirectInputToFile();
+            var line = Console.ReadLine();
+            while (!string.IsNullOrEmpty(line)) {
+                var firstArray = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries),
+                    int.Parse);
+                Console.ReadLine();
+                var secondArray = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries),
+                    int.Parse);
+                var firstSum = firstArray.Sum();
+                var secondSum = secondArray.Sum();
+                string answer;
+                if (firstSum > secondSum) {
+                    answer = string.Join(" ", firstArray);
+                }
+                else {
+                    answer = string.Join(" ", secondArray);
+                }
+
+                Console.WriteLine(answer);
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
