c3cf5620c2477f26a922641b958a6fa14c005989
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-04-08 17:45:18 +0200

    NabilHacker StackMetod

--- FunWithSequencesPartFour/FunWithSequencesPartFour.cs
+++ FunWithSequencesPartFour/FunWithSequencesPartFour.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace FunWithSequencesPartFour
-{
-    class FunWithSequencesPartFour
-    {
-        static void Main()
-        {
-            Console.WriteLine("Hello World!");
+namespace FunWithSequencesPartFour {
+    class FunWithSequencesPartFour {
+        static void Main() {
+            Helpers.ConsoleHelper.RedirectInputToFile();
+            var line = Console.ReadLine();
+            while (!string.IsNullOrEmpty(line)) {
+                var specifications =
+                    Array.ConvertAll(line.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+                var n = specifications[0];
+                var x = specifications[1];
+                var numbersS = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries),
+                    int.Parse);
+                var numbersQ = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries),
+                    int.Parse);
+                var answersList = new List<int>();
+                for (int i = 0; i < n; i++) {
+                    for (int mutableX = -x; mutableX <= x; mutableX++) {
+                        var positionQ = i + mutableX;
+                        if (positionQ < 1) {
+                            continue;
+                        }
+
+                        if (positionQ >= n) {
+                            break;
+                        }
+
+                        if (numbersS[i] == numbersQ[positionQ]) {
+                            answersList.Add(i + 1);
+                        }
+                    }
+                }
+
+                Console.WriteLine(string.Join(" ", answersList));
+                line = Console.ReadLine();
+            }
         }
     }
 }
---
+++ NabliHacher/NabilHacker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NabilHacker {
+    class NabilHacker {
+        static void Main() {
+            Helpers.ConsoleHelper.RedirectInputToFile();
+            var numberOfCases = Convert.ToInt32(Console.ReadLine());
+            while (numberOfCases != 0) {
+                var code = Console.ReadLine();
+                var mainStack = new Stack<char>();
+                var helperStack = new Stack<char>();
+                foreach (var sign in code) {
+                    if (sign == '>') {
+                        if (helperStack.Count == 0) {
+                            continue;
+                        }
+                        mainStack.Push(helperStack.Pop());
+                    }
+                    else if (sign == '<') {
+                        if (mainStack.Count == 0) {
+                            continue;
+                        }
+                        helperStack.Push(mainStack.Pop());
+                    }
+                    else if (sign == '-') {
+                        if (mainStack.Count == 0) {
+                            continue;
+                        }
+                        mainStack.Pop();
+                    }
+                    else {
+                        mainStack.Push(sign);
+                    }
+                }
+
+                foreach (var c in helperStack) {
+                    mainStack.Push(c);
+                }
+
+                var reverseStack = new char[mainStack.Count];
+                for (int i = mainStack.Count - 1; i >= 0; i--) {
+                    reverseStack[i] = mainStack.Pop();
+                }
+
+                var answer = string.Join("", reverseStack);
+                Console.WriteLine(answer);
+
+                numberOfCases--;
+            }
+        }
+    }
+}
