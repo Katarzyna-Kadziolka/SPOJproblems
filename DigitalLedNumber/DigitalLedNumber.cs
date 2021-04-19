43b18db9ac0a43e8d9d36f2ee3172a5580a1f797
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-04-19 19:00:07 +0200

    DigitalLedNumbers

---
+++ DigitalLedNumber/DigitalLedNumber.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+
+namespace DigitalLedNumber {
+    class DigitalLedNumber {
+        static void Main(string[] args) {
+            Helpers.ConsoleHelper.RedirectInputToFile();
+            var firstLine = Console.ReadLine();
+            while (!string.IsNullOrEmpty(firstLine)) {
+                var secondLine = Console.ReadLine();
+                var thirdLine = Console.ReadLine();
+                var sb = new StringBuilder();
+                for (int i = 0; i < firstLine.Length; i++) {
+                    if (firstLine[i] == ' ' && firstLine[i + 1] == ' ') {
+                        if (secondLine[i] == ' ') {
+                            sb.Append("1");
+                            i++;
+                            i++;
+                        }
+                        else {
+                            sb.Append("4");
+                            i++;
+                            i++;
+                        }
+                    }
+                    else if (secondLine[i] == ' ') {
+                        if (secondLine[i + 1] == ' ') {
+                            sb.Append("7");
+                            i++;
+                            i++;
+                        }
+                        else {
+                            if (thirdLine[i] == ' ') {
+                                sb.Append("3");
+                                i++;
+                                i++;
+                            }
+                            else {
+                                sb.Append("2");
+                                i++;
+                                i++;
+                            }
+                        }
+                    }
+                    else if (secondLine[i + 1] == ' ') {
+                        sb.Append("0");
+                        i++;
+                        i++;
+                    }
+                    else if (secondLine[i + 2] == ' ') {
+                        if (thirdLine[i] == ' ') {
+                            sb.Append("5");
+                            i++;
+                            i++;
+                        }
+                        else {
+                            sb.Append("6");
+                            i++;
+                            i++;
+                        }
+                    }
+                    else if (thirdLine[i] == ' ') {
+                        sb.Append("9");
+                        i++;
+                        i++;
+                    }
+                    else if (thirdLine[i] == '|') {
+                        sb.Append("8");
+                        i++;
+                        i++;
+                    }
+                }
+
+                Console.WriteLine(sb);
+                firstLine = Console.ReadLine();
+            }
+        }
+    }
+}
--- LedNumbers/LedNumbers.cs
+++ LedNumbers/LedNumbers.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 
-namespace LedNumbers
-{
-    class LedNumbers
-    {
-        static void Main(string[] args)
-        {
+namespace LedNumbers {
+    class LedNumbers {
+        static void Main(string[] args) {
             Helpers.ConsoleHelper.RedirectInputToFile();
             var line = Console.ReadLine();
             while (!string.IsNullOrEmpty(line)) {
@@ -14,70 +11,73 @@ namespace LedNumbers
                 var thirdLine = new string[line.Length];
                 for (int i = 0; i < line.Length; i++) {
                     switch (line[i]) {
-                        case '0':
+                        case'0':
                             firstLine[i] = " _ ";
                             secondLine[i] = "| |";
                             thirdLine[i] = "|_|";
                             break;
-                        case '1':
+                        case'1':
                             firstLine[i] = "   ";
                             secondLine[i] = "  |";
                             thirdLine[i] = "  |";
                             break;
-                        case '2':
+                        case'2':
                             firstLine[i] = " _ ";
                             secondLine[i] = " _|";
                             thirdLine[i] = "|_ ";
                             break;
-                        case '3':
+                        case'3':
                             firstLine[i] = " _ ";
                             secondLine[i] = " _|";
                             thirdLine[i] = " _|";
                             break;
-                        case '4':
+                        case'4':
                             firstLine[i] = "   ";
                             secondLine[i] = "|_|";
                             thirdLine[i] = "  |";
                             break;
-                        case '5':
+                        case'5':
                             firstLine[i] = " _ ";
                             secondLine[i] = "|_ ";
                             thirdLine[i] = " _|";
                             break;
-                        case '6':
+                        case'6':
                             firstLine[i] = " _ ";
                             secondLine[i] = "|_ ";
                             thirdLine[i] = "|_|";
                             break;
-                        case '7':
+                        case'7':
                             firstLine[i] = " _ ";
                             secondLine[i] = "  |";
                             thirdLine[i] = "  |";
                             break;
-                        case '8':
+                        case'8':
                             firstLine[i] = " _ ";
                             secondLine[i] = "|_|";
                             thirdLine[i] = "|_|";
                             break;
-                        case '9':
+                        case'9':
                             firstLine[i] = " _ ";
                             secondLine[i] = "|_|";
                             thirdLine[i] = "  |";
                             break;
                     }
                 }
-                
+
                 foreach (var s in firstLine) {
                     Console.Write(s);
                 }
+
                 Console.WriteLine("");
                 foreach (var s in secondLine) {
                     Console.Write(s);
                 }
+
                 Console.WriteLine("");
                 foreach (var s in thirdLine) {
                     Console.Write(s);
                 }
+
                 Console.WriteLine("");
                 line = Console.ReadLine();
             }
