f3e9a08b3240717683affaf3fb0e4a5d87c2832b
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-04-29 15:03:15 +0200

    RkSorting start

---
+++ RkSorting/RkSorting.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace RkSorting
+{
+    class RkSorting
+    {
+        static void Main(string[] args)
+        {
+            Helpers.ConsoleHelper.RedirectInputToFile();
+            var line = Console.ReadLine();
+            while (!string.IsNullOrEmpty(line)) {
+                var specifications =
+                    Array.ConvertAll(line.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+                var maxNum = specifications[1];
+                var numbers = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+                for (int i = 0; i < maxNum; i++) {
+                    var answer = new int[numbers.Length];
+                    var index = numbers.ToList().FindAll(a => a == i + 1).Count;
+                    if (index == 0) {
+                        continue;
+                    }
+                    else if (answer[index] != null) {
+                        answer[index + 1]
+                    }
+                }
+            }
+        }
+    }
+}
