0d82a088bb94a6752c15d2765fdab3a690165771
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-05-11 16:53:33 +0200

    RKSorting I don't know what I'm doing

--- RkSorting/RkSorting.cs
+++ RkSorting/RkSorting.cs
@@ -1,29 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 
-namespace RkSorting
-{
-    class RkSorting
-    {
-        static void Main(string[] args)
-        {
+namespace RkSorting {
+    class RkSorting {
+        static void Main(string[] args) {
             Helpers.ConsoleHelper.RedirectInputToFile();
             var line = Console.ReadLine();
             while (!string.IsNullOrEmpty(line)) {
                 var specifications =
                     Array.ConvertAll(line.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
                 var maxNum = specifications[1];
-                var numbers = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
-                for (int i = 0; i < maxNum; i++) {
-                    var answer = new int[numbers.Length];
-                    var index = numbers.ToList().FindAll(a => a == i + 1).Count;
-                    if (index == 0) {
-                        continue;
-                    }
-                    else if (answer[index] != null) {
-                        answer[index + 1]
+                var numbers = Array.ConvertAll(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries),
+                    int.Parse);
+                List<int> answer = new List<int>();
+
+                for (int i = 0; i < numbers.Length; i++) {
+                    if (!answer.Contains(numbers[i])) {
+                        var numOfRepeats = numbers.Count(a => a == numbers[i]);
+                        for (int j = 0; j < numOfRepeats; j++) {
+                            answer.Add(numbers[i]);
+                        }
                     }
                 }
+
+                Console.WriteLine(string.Join(" ", answer));
+                line = Console.ReadLine();
             }
         }
     }
