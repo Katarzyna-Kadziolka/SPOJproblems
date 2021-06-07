40520dc44256c166ec434506829ea9ec82638b84
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-06-07 18:57:47 +0200

    no message

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -1,30 +1,83 @@
 ﻿using System;
 using System.Linq;
 
-namespace Inheritance
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+namespace Inheritance {
+	public class Program {
+		static void Main(string[] args) {
+			try
+			{
+				Person p = new Person("Molenda", "Krzysztof", 18);
+				Console.WriteLine(p);
+			}
+			catch( Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+		}
+	}
 
-    public class Person {
-        public string FirstName { get;
-            set {
-                if (value.Any(char.IsLetter) || string.IsNullOrEmpty(value)) {
-                    throw new ArgumentException("Wrong Name!");
-                }
-                else {
-                    value = value.Trim();
-                    FirstName = char.ToUpper(value[0]) + value.Substring(1);
+	public class Person {
+		public string FirstName {
+			get { return FirstName; }
+			// protected
+			set {
+				if (!value.All(char.IsLetter) || string.IsNullOrEmpty(value)) {
+					throw new ArgumentException("Wrong Name!");
+				}
+				else {
+					value = value.Trim();
+					FirstName = char.ToUpper(value[0]) + value.Substring(1);
+				}
+			}
+		}
 
-                }
-            } }
-        public string LastName { get; set; }
-        public int Age { get; set; }
+		public string FamilyName {
+			get { return FamilyName; }
+			// protected 
+			set {
+				if (!value.All(char.IsLetter) || string.IsNullOrEmpty(value)) {
+					throw new ArgumentException("Wrong Name!");
+				}
+				else {
+					value = value.Trim();
+					FamilyName = char.ToUpper(value[0]) + value.Substring(1);
+				}
+			}
+		}
 
-    }
+		public int Age {
+			get { return Age; }
+			// protected 
+			set {
+				if (value < 0) {
+					throw new ArgumentException("Age must be positive!");
+				}
+				else {
+					Age = value;
+				}
+			}
+		}
+
+		public Person(string familyName, string firstName, int age) {
+			FamilyName = familyName;
+			FirstName = firstName;
+			Age = age;
+		}
+
+		public override string ToString() {
+			return $"{FirstName} {FamilyName} ({Age})";
+		}
+
+		public void modifyFirstName(string firstName) {
+			FirstName = firstName;
+		}
+
+		public void modifyFamilyName(string familyName) {
+			FamilyName = familyName;
+		}
+
+		public void modifyAge(int age) {
+			Age = age;
+		}
+	}
 }
