3f88ffac1a8e178a4efa7f830a1fff93e1456b9e
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-06-09 17:28:26 +0200

    account part two start

---
+++ AccountPartTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AccountPartTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- LogsAgregation/Program.cs
+++ LogsAgregation/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata.Ecma335;
 
 namespace LogsAgregation {
     public class Program {
-        static void Main(string[] args) {
+        static void Main() {
             Helpers.ConsoleHelper.RedirectInputToFile();
             var numberOfCases = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < numberOfCases; i++) {
