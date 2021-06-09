cfadb46b69bb3bd346e8c98ea74005622d6d2038
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-06-09 17:28:49 +0200

    no message

--- AccountPartTwo/Program.cs
+++ AccountPartTwo/Program.cs
@@ -9,4 +9,83 @@ namespace AccountPartTwo
             Console.WriteLine("Hello World!");
         }
     }
+
+    public interface IAccount
+    {
+        // nazwa klienta, bez spacji przed i po
+        // readonly - modyfikowalna wyłącznie w konstruktorze
+        string Name {get;}
+
+        // bilans, aktualny stan środków, podawany w zaokrągleniu do 2 miejsc po przecinku
+        decimal Balance {get;}
+
+        // czy konto jest zablokowane
+        bool IsBlocked { get; }
+        void Block();            // zablokowanie konta
+        void Unblock();          // odblokowanie konta
+
+        // wpłata, dla kwoty ujemnej - zignorowana (false)
+        // jeśli konto zablokowane - zignorowana (false)
+        // true jeśli wykonano i nastąpiła zmiana salda
+        bool Deposit(decimal amount);
+
+        // wypłata, dla kwoty ujemnej - zignorowana (false)
+        // jeśli konto zablokowane - zignorowana (false)
+        // jeśli jest niewystarczająca ilość środków - zignorowana (false)
+        // true jeśli wykonano i nastąpiła zmiana salda   
+        bool Withdrawal(decimal amount);
+    }
+
+    public interface IAccountWithLimit : IAccount
+    {
+        // przyznany limit debetowy
+        // mozliwość zmiany, jeśli konto nie jest zablokowane
+        decimal OneTimeDebetLimit { get; set; }
+
+        // dostępne środki, z uwzględnieniem limitu
+        decimal AvaibleFounds { get; } 
+    }
+
+    public class Account : IAccount
+    {
+        protected const int PRECISION = 4;
+
+        public string Name { get; }
+        public decimal Balance { get; private set; }
+
+
+        public bool IsBlocked { get; private set; } = false;
+        public void Block() => IsBlocked = true;
+        public void Unblock() => IsBlocked = false;
+
+        public Account(string name, decimal initialBalance = 0)
+        {
+            if (name == null || initialBalance < 0)
+                throw new ArgumentOutOfRangeException();
+            Name = name.Trim();
+            if (Name.Length < 3)
+                throw new ArgumentException();
+            Balance = Math.Round(initialBalance, PRECISION);
+        }
+
+        public bool Deposit(decimal amount)
+        {
+            if (amount <= 0 || IsBlocked) return false;
+
+            Balance = Math.Round(Balance += amount, PRECISION);
+            return true;
+        }
+
+        public bool Withdrawal(decimal amount)
+        {
+            if (amount <= 0 || IsBlocked || amount > Balance) return false;
+
+            Balance = Math.Round(Balance -= amount, PRECISION);
+            return true;
+        }
+
+        public override string ToString() =>
+            IsBlocked ? $"Account name: {Name}, balance: {Balance:F2}, blocked"
+                : $"Account name: {Name}, balance: {Balance:F2}";
+    }
 }
